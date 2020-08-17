daf14bc2d00a7ad8ecb2b5aff085d495c1b51141
chizzo <57238328+tsubachoko@users.noreply.github.com>
2020-08-17 18:17:03 +0900

    ユーザー登録機能の追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @items = @user.items
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "会員登録を完了しました"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'users/show'
   root 'items#index'
   
   resources :pairings, only: [:index, :show]
   resources :drinks, only: [:index, :show]
   resources :foods, only: [:index, :show]
 
+  resources :users
 
 end
